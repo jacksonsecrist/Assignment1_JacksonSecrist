0b9ba578f1b7790d380d6506dd630602e0d1fe57
Jackson Secrist <js311@byu.edu>
2021-01-27 18:20:59 -0700

    more stuff

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1_JacksonSecrist.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GradeCalculator()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/GradeCalculatorModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment1_JacksonSecrist.Models
+{
+    public class GradeCalculatorModel
+    {
+    }
+}
